4486f46e9c9588d4594e2cd84dfcec6517923722
Damian Tabaczynski <dam.tab@wp.pl>
2019-04-20 14:45:25 +0200

    Enable Gitignore

--- cmake-build-debug/MYRUN.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-command = "TSP_Metaheuristic.exe"
-instances=["bier127.txt","tsp250.txt","tsp500.txt","tsp1000.txt"]
-time="300"
-while(True):
-    for which in instances:
-        process = subprocess.call([command,which,time], shell=True)
